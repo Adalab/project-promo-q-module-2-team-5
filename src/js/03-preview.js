d362ebb5a9cb6dd9b156413105dfd61db4f38876
NadiaMonzon <nadiamonque99@gmail.com>
2022-07-04 18:40:49 +0100

    Fixed input bug

--- src/js/03-preview.js
+++ src/js/03-preview.js
@@ -16,21 +16,18 @@ const data = {
 
 const handleInput = (ev) => {
 
-    const nameInput = ev.target.name;
-    const valueInput = ev.target.value;
-    const id = document.getElementById(nameInput);
-    data[nameInput] = valueInput;
-    console.log(id);
-    if (nameInput === "name" || nameInput === "job") {
-        id.innerHTML = valueInput;
-    }
+  const nameInput = ev.target.name;
+  const valueInput = ev.target.value;
+  const id = document.getElementById(nameInput);
+  data[nameInput] = valueInput;
+  console.log(id);
+  if (nameInput === "name" || nameInput === "job") {
+    id.innerHTML = valueInput;
+  }
 
-    if (nameInput === "email" || nameInput === "tel" || nameInput === "linkedin" || nameInput === "github") {
-        id.href = valueInput;
-    }
-    if (nameInput === "profileImage") {
-        id.src = valueInput;
-    }
+  if (nameInput === "email" || nameInput === "tel" || nameInput === "LinkedIn" || nameInput === "github") {
+    id.href = valueInput;
+  }
 };
 
 allInputs.addEventListener('keyup', handleInput);
