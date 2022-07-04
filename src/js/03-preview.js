d0e5d8163ee553ff0ba7a658bb28dbb35e4bd794
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-07-04 13:30:09 +0200

    preview-update

--- src/js/03-preview.js
+++ src/js/03-preview.js
@@ -1,21 +1,5 @@
 "use strict";
 
-/*const inputName = document.querySelector ('.js_inputName');
-const inputJob = document.querySelector ('.js_inputJob');
-const inputImg = document.querySelector ('.js_inputImg');
-const inputEmail = document.querySelector ('.js_inputEmail');
-const inputTel = document.querySelector ('.js_inputTel');
-const inputLinkedin = document.querySelector ('.js_inputLinkedin');
-const inputGithub = document.querySelector ('.js_inputGithub');
-
-const previewName = document.querySelector ('.js_previewName');
-const previewJob = document.querySelector ('.js_previewJob');
-const previewImg = document.querySelector ('.js_previewImg');
-const previewTel = document.querySelector ('.js_previewTel');
-const previewEmail = document.querySelector ('.js_previewEmail');
-const previewLinkedin = document.querySelector ('.js_previewLinkedin');
-const previewGithub = document.querySelector ('.js_previewGithub');*/
-
 const allInputs = document.querySelector(".js_allInputs");
 
 
@@ -37,13 +21,16 @@ const handleInput = (ev) => {
     const id = document.getElementById(nameInput);
     data[nameInput] = valueInput;
     console.log(id);
+    if (nameInput === "name" || nameInput === "job") {
+        id.innerHTML = valueInput;
+    }
 
-
-
-    id.innerHTML = valueInput;
-
-
-
+    if (nameInput === "email" || nameInput === "tel" || nameInput === "linkedin" || nameInput === "github") {
+        id.href = valueInput;
+    }
+    if (nameInput === "profileImg") {
+        id.src = valueInput;
+    }
 };
 
 allInputs.addEventListener('keyup', handleInput);
